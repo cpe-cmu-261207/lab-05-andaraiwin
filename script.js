b3e254287f152c348c88d3cdb55df81a2a4a0b55
Raiwin Inthasit <andaraiwin@gmail.com>
2022-08-06 19:30:36 +0700

    update code

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const inputAdd = document.getElementById("input-add-todo");
 const todoCtn = document.getElementById("todo-container");
+const localStorageName = "setyorsoodtorsoodporcmorsonghorsaikai";
 
 inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
-
-  //your code here
+  const val = inputAdd.value;
+  if (val == "") {
+    alert("Todo cannot be empty");
+    return;
+  }
+  addTodo(val, false);
+  inputAdd.value = "";
+  saveTodo();
 };
 
 function addTodo(title, completed) {
@@ -29,20 +36,48 @@ function addTodo(title, completed) {
   deleteBtn.className = "btn btn-danger";
 
   //your code here
-  //append todo to HTML...
-  //define buttons event...
+  div.append(span, doneBtn, deleteBtn);
+
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+  doneBtn.onclick = () => {
+    completed = !completed;
+    span.style.textDecoration = completed ? "line-through" : "";
+    saveTodo();
+  };
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
+  todoCtn.insertBefore(div, todoCtn.firstChild);
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
-    //your code here
+    data.push({
+      title: todoDiv.children[0].innerText,
+      completed: todoDiv.children[0].style.textDecoration === "line-through",
+    });
   }
-  //your code here
+  localStorage.setItem(localStorageName, JSON.stringify(data.reverse()));
 }
 
 function loadTodo() {
-  //your code here
+  const raw = localStorage.getItem(localStorageName);
+  if (raw == null) return;
+  const data = JSON.parse(raw);
+  for (const todo of data) {
+    addTodo(todo.title, todo.completed);
+  }
 }
 
 loadTodo();
